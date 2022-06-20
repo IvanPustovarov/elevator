5a1b5182b0f9f015b51f99fb82317f4f19ef3fe8
WIllsnSmith <silverflash150@gmail.com>
2022-06-20 13:14:24 +0300

    styled elevator

---
+++ src/components/Elevator.vue
@@ -0,0 +1,17 @@
+<style scoped>
+.cell {
+  outline: 1px solid blue;
+  width: 120px;
+  text-align: center;
+  vertical-align: middle;
+}
+tr {
+  height: 150px;
+}
+
+.elevator {
+  width: 120px;
+  height: 150px;
+  background-color: rgb(86, 145, 110);
+}
+</style>
