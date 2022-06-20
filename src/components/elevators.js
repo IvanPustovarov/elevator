b54e0691a85e37fd2062447c20cadc4d41c7ba96
WIllsnSmith <silverflash150@gmail.com>
2022-06-20 13:23:07 +0300

    elevator manager & elevator

--- src/components/elevators.js
+++ src/components/elevators.js
@@ -3,3 +3,12 @@ const WAITING = 3000;
 const wait = async (milliseconds) =>
   new Promise((resolve) => setTimeout(resolve, milliseconds));
 
+class Elevator {
+  constructor() {
+    this.position = 1;
+    this.status = "IDLE";
+    this.floorCalls = [];
+  }
+}
+export default class ElevatorsManager {
+}
