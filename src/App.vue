930e93d5bd4f168f45275b36dcf3101252f80cfe
WIllsnSmith <silverflash150@gmail.com>
2022-06-20 13:12:19 +0300

    create app.vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ElevatorComponent />
+  </div>
+</template>
+
+<script>
+import ElevatorComponent from "./components/Elevator.vue";
+
+export default {
+  name: "App",
+  components: {
+    ElevatorComponent,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
